db6d4f6ca8b0b1d855ad448c2c240d14bfeb3c47
aloksingh1st <aloksingh1st@gmail.com>
2023-05-15 23:45:13 +0530

    may 15

---
+++ Linked_List/Add_two_Linked_list_as_LL_II.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode
+{
+
+public:
+    int data;
+    ListNode *next;
+
+    ListNode(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+
+    ~ListNode()
+    {
+        int val = this->data;
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
+        }
+
+        cout << "Memory is free for node with data " << val << endl;
+    }
+};
+
+void insertAtHead(ListNode *&head, int data)
+{
+
+    // create new node
+
+    ListNode *newNode = new ListNode(data);
+
+    newNode->next = head;
+    head = newNode;
+}
+
+void printLL(ListNode *&head)
+{
+    ListNode *newNode = head;
+
+    while (newNode != NULL)
+    {
+        cout << " " << newNode->data << " --> ";
+        newNode = newNode->next;
+    }
+    cout << endl;
+}
+
+int printInt(ListNode *&head)
+{
+    ListNode *newNode = head;
+    int toExport = 0;
+
+    while (newNode != NULL)
+    {
+        // cout << " " << newNode->data << " --> ";
+        toExport = toExport * 10 + newNode->data;
+        newNode = newNode->next;
+    }
+    // cout << endl;
+    return toExport;
+}
+
+void Reverse(ListNode *&head)
+{
+    ListNode *current = head;
+    ListNode *prev = NULL, *next = NULL;
+
+    while (current != NULL)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    head = prev;
+}
+
+ListNode *addTwoLists(ListNode *first, ListNode *second)
+{
+
+    int a = printInt(first);
+    int b = printInt(second);
+
+    int n = a + b;
+    ListNode *ans = new ListNode(n % 10);
+    n = n / 10;
+
+    while (n != 0)
+    {
+        // temp = n%10;
+        insertAtHead(ans, n % 10);
+        n = n / 10;
+    }
+
+    return ans;
+}
+
+ListNode *intToLl(int v)
+{
+    int n = v;
+
+    ListNode *ans = new ListNode(n % 10);
+    n = n / 10;
+
+    while (n != 0)
+    {
+        // temp = n%10;
+        insertAtHead(ans, n % 10);
+        n = n / 10;
+    }
+
+    return ans;
+}
+
+int main()
+{
+
+    ListNode *head = new ListNode(5);
+    // insertAtHead(head, 5);
+
+    insertAtHead(head, 4);
+    insertAtHead(head, 3);
+    insertAtHead(head, 2);
+    insertAtHead(head, 1);
+
+    ListNode *head2 = new ListNode(5);
+    insertAtHead(head2, 4);
+    
+    ListNode * head3 = addTwoLists(head, head2);
+
+    printLL(head3);
+
+    return 0;
+}
---
+++ Linked_List/last_Appearance.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode {
+
+    public:
+    int data;
+    ListNode * next;
+
+    ListNode(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+
+    ~ListNode()
+       {
+        int val = this->data;
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
+        }
+
+        cout << "Memory is free for node with data " << val << endl;
+    }
+};
+
+void insertAtHead(ListNode *&head, int data)
+{
+
+    // create new node
+
+    ListNode *newNode = new ListNode(data);
+
+    newNode->next = head;
+    head = newNode;
+}
+
+
+void printLL(ListNode *&head)
+{
+    ListNode *newNode = head;
+
+    while (newNode != NULL)
+    {
+        cout << " " << newNode->data << " --> ";
+        newNode = newNode->next;
+    }
+    cout << endl;
+}
+
+
+void Reverse(ListNode * &head){
+    ListNode* current = head;
+    ListNode* prev = NULL, *next = NULL;
+
+    while(current != NULL){
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    head = prev;
+}
+
+
+ListNode *lastAppearance(ListNode *head) { 
+	// Write your code here.
+  head = Reverse(head);
+
+  unordered_map<int, int> m;
+
+  ListNode *temp = head;
+  ListNode *p = head;
+
+  while (temp != NULL) {
+    if (m[temp->data] == 1) {
+      p->next = temp->next;
+    } else {
+      m[temp->data] = 1;
+      p = temp;
+    }
+    temp = temp->next;
+  }
+  
+  head = Reverse(head);
+  return head;
+}
+
+
+int main(){
+
+    ListNode * head = new ListNode(10);
+    insertAtHead(head, 11);
+    insertAtHead(head, 12);
+    insertAtHead(head, 13);
+    insertAtHead(head, 14);
+    insertAtHead(head, 15);
+
+
+    // LastNode(head)->data<<"--------------------------------"<<endl;
+
+    printLL(head);
+
+    Reverse(head);
+
+    printLL(head);
+
+
+
+    return 0;
+}
