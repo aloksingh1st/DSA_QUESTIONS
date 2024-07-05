42927a7e16e017b87fe1656b2477ee8e0d1ca6d5
Alok Singh <85346311+aloksingh1st@users.noreply.github.com>
2024-07-05 20:39:03 +0530

    Add find-pivot-index.cpp

---
+++ find-pivot-index.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        vector<int>lsum(nums.size(), 0);
+        vector<int>rsum(nums.size(), 0);
+
+
+        for(int i=1; i<nums.size(); i++){
+            lsum[i] = lsum[i-1] + nums[i-1];
+        }
+
+        for( int i=nums.size()-2; i>=0; i--){
+            rsum[i] = rsum[i+1] + nums[i+1];
+        }
+
+        for(int i = 0; i<nums.size(); i++){
+            if(rsum[i] == lsum[i]){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
