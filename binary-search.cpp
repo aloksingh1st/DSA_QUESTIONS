918ede4e4d73710c21d3a00ac4421a7c489d26b2
Alok Singh <85346311+aloksingh1st@users.noreply.github.com>
2024-09-03 16:31:05 +0530

    Add binary-search.cpp

---
+++ binary-search.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int i =0, j = nums.size()-1;
+
+       while(j >= i){
+        int mid = i +( j - i) /2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] > target){
+                j = mid-1;
+            }
+
+            else{
+                i = mid+1;
+            }
+        }
+        return -1;
+    }
+};
