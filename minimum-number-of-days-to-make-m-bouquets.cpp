77a72fc438360dd6b5fd0587341c59990d25fa74
Alok Singh <85346311+aloksingh1st@users.noreply.github.com>
2024-09-22 19:56:28 +0530

    Add minimum-number-of-days-to-make-m-bouquets.cpp

---
+++ minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+
+ int getMax(vector<int>& bloomDay){
+    int ans= INT_MIN;
+
+    for(int i=0; i<bloomDay.size(); i++){
+        ans = max(ans, bloomDay[i]);
+    }
+
+    return ans;
+ }
+
+ bool possible(vector<int> &arr, int day, int m, int k) {
+    int n = arr.size(); //size of the array
+    int cnt = 0;
+    int noOfB = 0;
+    // count the number of bouquets:
+    for (int i = 0; i < n; i++) {
+        if (arr[i] <= day) {
+            cnt++;
+        }
+        else {
+            noOfB += (cnt / k);
+            cnt = 0;
+        }
+    }
+    noOfB += (cnt / k);
+    return noOfB >= m;
+}
+
+    int minDays(vector<int>& arr, int m, int k) {
+        long long val = m * 1ll * k * 1ll;
+    int n = arr.size(); //size of the array
+    if (val > n) return -1; //impossible case.
+    //find maximum and minimum:
+    int mini = INT_MAX, maxi = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        mini = min(mini, arr[i]);
+        maxi = max(maxi, arr[i]);
+    }
+
+    //apply binary search:
+    int low = mini, high = maxi;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (possible(arr, mid, m, k)) {
+            high = mid - 1;
+        }
+        else low = mid + 1;
+    }
+    return low;
+    }
+};
