1e3d9f759b8edf429d4b2a53eb5be23367c0d6a9
Alok Singh <85346311+aloksingh1st@users.noreply.github.com>
2024-11-11 19:47:23 +0530

    Add insert-interval.cpp

---
+++ insert-interval.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> res;
+        res.push_back(intervals[0]);
+
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (res.back()[1] >= intervals[i][0]) {
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
+            } else {
+                res.push_back(intervals[i]);
+            }
+        }
+
+        return res;        
+    }
+};
