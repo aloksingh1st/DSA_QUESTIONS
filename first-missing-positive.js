5e0911789843eb67bc6a5ad7795a5946d5ead3d9
Alok Singh <85346311+aloksingh1st@users.noreply.github.com>
2025-03-26 17:25:49 +0530

    Add first-missing-positive.js

---
+++ first-missing-positive.js
@@ -0,0 +1,14 @@
+var firstMissingPositive = function(nums) {
+    nums.sort((a, b) => a - b);
+
+    let missing = 1;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0 && nums[i] === missing) {
+        missing++;
+        } else if (nums[i] > missing) {
+        return missing;
+        }
+    }
+
+    return missing; 
+};
